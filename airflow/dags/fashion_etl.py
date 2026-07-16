f807a3b86d1bc1078d24a2535dd1b8940cc6a3d8
esnanta <ombakrinai@gmail.com>
2026-07-16 21:29:55 +0700

    update dag structure and add extract and validate tasks

--- airflow/dags/fashion_etl.py
+++ airflow/dags/fashion_etl.py
@@ -1,6 +1,11 @@
 from __future__ import annotations
 from datetime import datetime
 from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+from airflow.dags.tasks.validate import validate_task
+from tasks.extract import extract_task
+
 
 with DAG(
     dag_id="fashion_etl",
@@ -9,5 +14,16 @@ with DAG(
     schedule=None,
     catchup=False,
     tags={"fashion", "etl", "learning"},
-):
-    pass
+) as dag:
+
+    extract = PythonOperator(
+        task_id="extract",
+        python_callable=extract_task,
+    )
+
+    validate = PythonOperator(
+        task_id="validate",
+        python_callable=validate_task,
+    )
+
+    extract >> validate
