f0fdca1edaca4dc7432ea15eef7f9b7d492e5e7c
esnanta <ombakrinai@gmail.com>
2026-07-16 21:29:28 +0700

    update extract_task to return DatasetArtifact instead of str

--- airflow/dags/tasks/extract.py
+++ airflow/dags/tasks/extract.py
@@ -4,14 +4,17 @@ from pipeline.extract import (
     DEFAULT_BASE_URL,
     scrape_data,
 )
-from pipeline.storage import save_raw_dataset
+from pipeline.storage import (
+    DatasetArtifact,
+    save_raw_dataset,
+)
 
 DATASET_NAME = "products"
 
 
-def extract_task() -> str:
+def extract_task() -> DatasetArtifact:
     """
-    Extract data from website and save it
+    Extract data from a website and save it
     as a Raw Dataset Artifact.
     """
     rows = scrape_data(DEFAULT_BASE_URL)
@@ -21,4 +24,4 @@ def extract_task() -> str:
         dataset=DATASET_NAME,
     )
 
-    return str(artifact.path)
+    return artifact
