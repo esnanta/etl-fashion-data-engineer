1e51be7c1d2ad1cabd28eced64532fdada19bcf1
esnanta <ombakrinai@gmail.com>
2026-07-17 17:55:40 +0700

    update

--- activate-airflow.sh
+++ activate-airflow.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-# source activate-dags.sh
+# source activate-airflow.sh
 
 source myvenv/bin/activate
+
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 export AIRFLOW_HOME="$PROJECT_ROOT/orchestration"
-export PYTHONPATH=$PROJECT_ROOT
+export PYTHONPATH="$PROJECT_ROOT"
+
+echo "PROJECT_ROOT=$PROJECT_ROOT"
 echo "VIRTUAL_ENV=$VIRTUAL_ENV"
 echo "AIRFLOW_HOME=$AIRFLOW_HOME"
+echo "PYTHONPATH=$PYTHONPATH"
