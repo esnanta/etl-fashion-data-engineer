b47ddc7ec6f82fbda666b21914c6c138031eafa3
esnanta <ombakrinai@gmail.com>
2026-07-17 17:36:32 +0700

    update

--- activate-airflow.sh
+++ activate-airflow.sh
@@ -3,6 +3,6 @@
 
 source myvenv/bin/activate
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export AIRFLOW_HOME="$PROJECT_ROOT/airflow"
+export AIRFLOW_HOME="$PROJECT_ROOT/orchestration"
 echo "VIRTUAL_ENV=$VIRTUAL_ENV"
 echo "AIRFLOW_HOME=$AIRFLOW_HOME"
